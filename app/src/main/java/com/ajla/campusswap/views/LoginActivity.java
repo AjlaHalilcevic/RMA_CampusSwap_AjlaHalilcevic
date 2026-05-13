1875af5f19924464d4954190c6b40f3935a3300c
Ajla Halilcevic <ahalilcevic04@gmail.com>
2026-05-13 02:48:44 +0200

    Add LoginActivity layout and initialize Firebase Auth

---
+++ app/src/main/java/com/ajla/campusswap/views/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.ajla.campusswap.views;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.ajla.campusswap.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText emailInput, passwordInput;
+    private Button loginButton;
+    private TextView registerLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+       // mAuth = FirebaseAuth.getInstance();
+        emailInput = findViewById(R.id.emailInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        loginButton = findViewById(R.id.loginButton);
+        registerLink = findViewById(R.id.registerLink);
+    }
+}
